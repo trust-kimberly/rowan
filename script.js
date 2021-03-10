676314e9aa1aac83c92ccd1bf91aa1ca51d8c781
Kimberly Nguyen <62153141+trust-kimberly@users.noreply.github.com>
2021-03-10 13:36:00 -0500

    thumbnails

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 $(window).scroll(function() {
     if($(window).scrollTop() + $(window).height() >= $(document).height()) {
-        $('body').css("color", "blue");
+        $('body').css("color", "#ff3c00");
+        $('body').css("background-color", "black");
     } 
    if($(window).scrollTop() + $(window).height() < $(document).height()) {
         $('body').css("color", "white");
@@ -10,50 +11,71 @@ $(window).scroll(function() {
  });
 
 // change project thumbnail image on hover
- 
+
 $(document).ready(function(){
+    $('#title-1').hover(function() {
+        $( '.image' ).css("background-image", "url(media/rowan-1.jpg)");
+        $('body').css("color", "#ff3c00");
+        $('body').css("background-color", "black");
+        });   
+    $('#title0').hover(function() {
+        $( '.image' ).css("background-image", "url(media/rowan0.jpg)");
+        $('body').css("color", "peachpuff");
+        $('body').css("background-color", "#e5454c");
+        });      
     $('#title1').hover(function() {
         $( '.image' ).css("background-image", "url(media/rowan1.jpg)");
-        $('body').css("background-color", "peachpuff");
+        $('body').css("color", "aliceblue");
+        $('body').css("background-color", "#aba99c");
         });        
     $('#title2').hover(function() {
         $( '.image' ).css("background-image", "url(media/rowan2.jpg)");
-        $('body').css("background-color", "aliceblue");
+        $('body').css("color", "#6f6e60");
+        $('body').css("background-color", "#c7ceca");
         });
     $('#title3').hover(function() {
         $( '.image' ).css("background-image", "url(media/rowan3.jpeg)");
-        $('body').css("background-color", "darkcyan");
+        $('body').css("color", "#32327d");
+        $('body').css("background-color", "whitesmoke");
         });
     $('#title4').hover(function() {
         $( '.image' ).css("background-image", "url(media/rowan4.jpg)");
-        $('body').css("background-color", "floralwhite");
+        $('body').css("color", "lavender");
+        $('body').css("background-color", "#161f33");
         });
     $('#title5').hover(function() {
         $( '.image' ).css("background-image", "url(media/rowan5.jpg)");
-        $('body').css("background-color", "mistyrose");
+        $('body').css("color", "#f1c5b3");
+        $('body').css("background-color", "#481603");
         });
     $('#title6').hover(function() {
         $( '.image' ).css("background-image", "url(media/rowan6.jpg)");
-        $('body').css("background-color", "rosybrown");
+        $('body').css("color", "whitesmoke");
+        $('body').css("background-color", "#b5bbbb");
         });
     $('#title7').hover(function() {
         $( '.image' ).css("background-image", "url(media/rowan7.jpg)");
-        $('body').css("background-color", "thistle");
+        $('body').css("color", "black");
+        $('body').css("background-color", "whitesmoke");
         });
     $('#title8').hover(function() {
         $( '.image' ).css("background-image", "url(media/rowan8.jpg)");
-        $('body').css("background-color", "whitesmoke");
+        $('body').css("color", "#b7cfd6");
+        $('body').css("background-color", "#2b2c26");
         });
     $('#title9').hover(function() {
         $( '.image' ).css("background-image", "url(media/rowan9.jpg)");
-        $('body').css("background-color", "teal");
+        $('body').css("color", "#d2c5a7");
+        $('body').css("background-color", "#2b2e4c");
         });
     $('#title10').hover(function() {
         $( '.image' ).css("background-image", "url(media/rowan10.jpg)");
-        $('body').css("background-color", "plum");
+        $('body').css("color", "#6c8280");
+        $('body').css("background-color", "#d6cec1");
         });
     $('#title11').hover(function() {
         $( '.image' ).css("background-image", "url(media/rowan11.jpeg)");
+        $('body').css("color", "aliceblue");
         $('body').css("background-color", "navy");
         });
     $('#title12').hover(function() {
@@ -62,6 +84,7 @@ $(document).ready(function(){
         });
     $('#title13').hover(function() {
         $( '.image' ).css("background-image", "url(media/rowan13.jpg)");
+        $('body').css("color", "aliceblue");
         $('body').css("background-color", "darkslategray");
         });				
 });
